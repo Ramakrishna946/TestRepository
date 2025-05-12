30da1d66fd879f9971b2cb29e3bd8b534dcb8cfd
Ramakrishna946 <chkri@RamakrishnaC>
2025-05-12 10:32:01 +0530

    commit on 12/05/2025 10:31

--- Practice/SeleniumPractice/src/test/java/day03052024/SSlCheck.java
+++ Practice/SeleniumPractice/src/test/java/day03052024/SSlCheck.java
@@ -7,31 +7,45 @@ import java.time.Duration;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SSlCheck {
 
-	public static void main(String[] args) throws IOException {
-		ChromeOptions options= new ChromeOptions();
-		options.setAcceptInsecureCerts(true);
-		options.addArguments("--incognito");
-		WebDriverManager.chromedriver().setup();
+    public static void main(String[] args) throws IOException {
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        options.addArguments("--incognito");
+
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://expired.badssl.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().deleteAllCookies();
-        System.out.println(driver.getTitle());
-        File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        String time=new SimpleDateFormat("ddMMyyyy_HHmmss").format(new Date());
-        FileUtils.copyFile(src, new File("C:\\Users\\chkri\\git\\repository2\\Practice\\SeleniumPractice\\Screenshots\\Screenshots_" + time + ".png"));
-        driver.quit();
-	}
 
+        System.out.println(driver.getTitle());
+
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String time = new SimpleDateFormat("ddMMyyyy_HHmmss").format(new Date());
+        FileUtils.copyFile(src, new File("C:\\Users\\chkri\\git\\repository2\\Practice\\SeleniumPractice\\Screenshots\\Screenshots_" + time + ".png"));
+
+        // Example usage of relative locators (update as needed for real elements on the page)
+        // Find a reference element first
+        // For demo purpose only (this site may not have an <input> or clear layout)
+        // Replace below with real element locators if needed
+//        WebElement referenceElement = driver.findElement(By.tagName("h1"));
+//        WebElement inputAbove = driver.findElement(RelativeLocator.with(By.tagName("input")).above(referenceElement));
+//        driver.findElement(RelativeLocator.with(By.tagName("")).)
+        driver.quit();
+    }
 }
--- Practice/SeleniumPractice/src/test/java/day24042025/Dropdown1.java
+++ Practice/SeleniumPractice/src/test/java/day24042025/Dropdown1.java
@@ -14,10 +14,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-@Test
+
 public class Dropdown1 {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
